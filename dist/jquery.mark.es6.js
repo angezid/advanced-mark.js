2d5a884b5d23d57785828abeadd7e0841a345c2d
angezid <82150610+angezid@users.noreply.github.com>
2022-10-04 14:07:59 +0800

    added ability to mark shadow DOM with any nested level (shadow root must have 'open' mode), tests
    some renaming
    
    Update mark.js
    
    run build
    
    Update mark.js
    
    run build

--- dist/jquery.mark.es6.js
+++ dist/jquery.mark.es6.js
@@ -8,11 +8,12 @@
 import './jquery.js';
 
 class DOMIterator {
-  constructor(ctx, iframes = true, exclude = [], iframesTimeout = 5000) {
+  constructor(ctx, iframes = true, exclude = [], iframesTimeout = 5000, shadowDOM = false) {
     this.ctx = ctx;
     this.iframes = iframes;
     this.exclude = exclude;
     this.iframesTimeout = iframesTimeout;
+    this.shadowDOM = shadowDOM;
   }
   static matches(element, selector) {
     const selectors = typeof selector === 'string' ? [selector] : selector,
@@ -166,6 +167,42 @@ class DOMIterator {
   createIterator(ctx, whatToShow, filter) {
     return document.createNodeIterator(ctx, whatToShow, filter, false);
   }
+  collectNodes(ctx, whatToShow, filterCb) {
+    const elements = [],
+      itr = this.createIterator(ctx, whatToShow, filterCb);
+    let node;
+    while ((node = itr.nextNode())) {
+      elements.push(node);
+    }
+    return elements;
+  }
+  collectNodesIncludeShadowDOM(ctx, whatToShow, filterCb) {
+    const elements = [],
+      showText = whatToShow === NodeFilter.SHOW_TEXT;
+    const loop = node => {
+      while (node) {
+        if (node.nodeType === Node.ELEMENT_NODE) {
+          if ( !showText && filterCb(node) === NodeFilter.FILTER_ACCEPT) {
+            elements.push(node);
+          }
+          if (node.shadowRoot && node.shadowRoot.mode === 'open') {
+            let elem = node.shadowRoot.querySelector(':first-child');
+            if (elem) {
+              loop(elem);
+            }
+          }
+        } else if (node.nodeType === Node.TEXT_NODE && showText && filterCb(node) === NodeFilter.FILTER_ACCEPT) {
+          elements.push(node);
+        }
+        if (node.hasChildNodes()) {
+          loop(node.firstChild);
+        }
+        node = node.nextSibling;
+      }
+    };
+    loop(ctx.firstChild);
+    return elements;
+  }
   createInstanceOnIframe(contents) {
     return new DOMIterator(contents.querySelector('html'), this.iframes);
   }
@@ -259,10 +296,10 @@ class DOMIterator {
         });
         elements.push(node);
       }
+    } else if (this.shadowDOM) {
+      elements = this.collectNodesIncludeShadowDOM(ctx, whatToShow, filterCb);
     } else {
-      while ((node = itr.nextNode())) {
-        elements.push(node);
-      }
+      elements = this.collectNodes(ctx, whatToShow, filterCb);
     }
     elements.forEach(node => {
       eachCb(node);
@@ -537,7 +574,8 @@ class Mark {
       this.ctx,
       this.opt.iframes,
       this.opt.exclude,
-      this.opt.iframesTimeout
+      this.opt.iframesTimeout,
+      this.opt.shadowDOM
     );
   }
   log(msg, level = 'debug') {
@@ -1416,13 +1454,7 @@ class Mark {
         this[fn](regex, 1, (t, node, filterInfo) => {
           return this.opt.filter(node, term, totalMarks, matches, filterInfo);
         }, (element, matchInfo) => {
-          if (across) {
-            if (matchInfo.matchStart) {
-              matches++;
-            }
-          } else {
-            matches++;
-          }
+          matches++;
           totalMarks++;
           this.opt.each(element, matchInfo);
         }, (count) => {
